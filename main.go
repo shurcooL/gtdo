a363a05b0db55b88c3e3edecda2350c97c0bbdfa
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-08 13:30:33 -0800

    Add GA for having some sense of levels of traffic.
    So that I could provide a better experience and decide how much time
    and effort to spend on the project.

--- main.go
+++ main.go
@@ -154,6 +154,16 @@ func codeHandler(w http.ResponseWriter, req *http.Request) {
 				transition: background 0.5s ease-in-out;
 			}
 		</style>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-56541369-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 	</head>
 	<body>
 		<div style="position: relative; min-height: 100%;">
