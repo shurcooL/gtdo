61ec50dd8c9d636a3ee4338562e14602a7e22b84
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-26 21:03:02 -0800

    Add a test for ImportPathElementsHtml generation code.
    Compare output with previous html/template approach for now.

--- main.go
+++ main.go
@@ -184,33 +184,7 @@ func codeHandler(w http.ResponseWriter, req *http.Request) {
 			data.Folders = append(data.Folders, fi.Name())
 		}
 
-		{
-			// Elements of importPath, first element being repoImportPath.
-			// E.g., {"github.com/user/repo", "subpath", "package"}.
-			elements := []string{repoImportPath}
-			elements = append(elements, strings.Split(importPath[len(repoImportPath):], "/")[1:]...)
-
-			var ns []*html.Node
-			for i, element := range elements {
-				if i != 0 {
-					ns = append(ns, html_gen.Text("/"))
-				}
-
-				path := path.Join(elements[:i+1]...)
-
-				// Don't link last importPath element, since it's the current page.
-				if path != importPath {
-					ns = append(ns, html_gen.A(element, template.URL("/"+path)))
-				} else {
-					ns = append(ns, html_gen.Text(element))
-				}
-			}
-
-			data.ImportPathElements, err = html_gen.RenderNodes(ns...)
-			if err != nil {
-				panic(err)
-			}
-		}
+		data.ImportPathElements = ImportPathElementsHtml(repoImportPath, importPath)
 	}
 
 	if bpkg != nil {
@@ -494,6 +468,37 @@ func buildContextUsingFS(fs vfs.FileSystem) build.Context {
 	}
 
 	return context
+}
+
+// ---
+
+func ImportPathElementsHtml(repoImportPath, importPath string) template.HTML {
+	// Elements of importPath, first element being repoImportPath.
+	// E.g., {"github.com/user/repo", "subpath", "package"}.
+	elements := []string{repoImportPath}
+	elements = append(elements, strings.Split(importPath[len(repoImportPath):], "/")[1:]...)
+
+	var ns []*html.Node
+	for i, element := range elements {
+		if i != 0 {
+			ns = append(ns, html_gen.Text("/"))
+		}
+
+		path := path.Join(elements[:i+1]...)
+
+		// Don't link last importPath element, since it's the current page.
+		if path != importPath {
+			ns = append(ns, html_gen.A(element, template.URL("/"+path)))
+		} else {
+			ns = append(ns, html_gen.Text(element))
+		}
+	}
+
+	importPathElements, err := html_gen.RenderNodes(ns...)
+	if err != nil {
+		panic(err)
+	}
+	return importPathElements
 }
 
 // ---
---
+++ main_test.go
@@ -0,0 +1,75 @@
+package main_test
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"path"
+	"strings"
+
+	gtdo "github.com/shurcooL/gtdo"
+)
+
+func ExampleImportPathElementsHtml() {
+	inputs := []struct {
+		repoImportPath string
+		importPath     string
+	}{
+		{"github.com/shurcooL/go", "github.com/shurcooL/go/u/u10"},
+		{"rsc.io/pd&f", "rsc.io/pd&f"},
+		{"rsc.io/pdf", "rsc.io/pdf"},
+		{"rsc.io/pdf", "rsc.io/pdf/pdfpasswd"},
+	}
+
+	for _, i := range inputs {
+		out1 := previousHtmlTemplateApproach(i.repoImportPath, i.importPath)
+		out2 := gtdo.ImportPathElementsHtml(i.repoImportPath, i.importPath)
+
+		if out1 != out2 {
+			panic(fmt.Errorf("out1 != out2\n%q\n%q\n", out1, out2))
+		}
+
+		fmt.Println(out1)
+	}
+
+	// Output:
+	//<a href="/github.com/shurcooL/go">github.com/shurcooL/go</a>/<a href="/github.com/shurcooL/go/u">u</a>/u10
+	//rsc.io/pd&amp;f
+	//rsc.io/pdf
+	//<a href="/rsc.io/pdf">rsc.io/pdf</a>/pdfpasswd
+}
+
+func previousHtmlTemplateApproach(repoImportPath, importPath string) template.HTML {
+	data := struct {
+		ImportPathElements [][2]string // Element name, and full path to element.
+	}{}
+
+	{
+		elements := strings.Split(importPath, "/")
+		elements = elements[len(strings.Split(repoImportPath, "/")):]
+
+		data.ImportPathElements = [][2]string{
+			[2]string{repoImportPath, repoImportPath},
+		}
+		for i, e := range elements {
+			data.ImportPathElements = append(data.ImportPathElements,
+				[2]string{e, repoImportPath + "/" + path.Join(elements[:i+1]...)},
+			)
+		}
+		// Don't link the last element, since it's the current page.
+		data.ImportPathElements[len(data.ImportPathElements)-1][1] = ""
+	}
+
+	t, err := template.New("import-path.html.tmpl").Parse(`{{define "ImportPath"}}{{range $i, $v := .}}{{if $i}}/{{end}}{{if (index $v 1)}}<a href="/{{(index $v 1)}}">{{(index $v 0)}}</a>{{else}}{{(index $v 0)}}{{end}}{{end}}{{end}}`)
+	if err != nil {
+		panic(err)
+	}
+
+	var buf bytes.Buffer
+	err = t.ExecuteTemplate(&buf, "ImportPath", data.ImportPathElements)
+	if err != nil {
+		panic(err)
+	}
+
+	return template.HTML(buf.String())
+}
