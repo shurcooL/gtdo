9d2a77ab5a349f2d952fce5e082ed094caad2ba2
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-30 02:02:19 -0800

    Add vcsstorecmd.
    It's a fork of vcsstore that replaces the git backend with gitcmd one.
    Fixes #6.

---
+++ cmd/vcsstorecmd/generate.go
@@ -0,0 +1,5 @@
+//go:generate go run gen.go
+
+// Command vcsstorecmd is a fork of sourcegraph.com/sourcegraph/vcsstore/cmd/vcsstore,
+// but it uses gitcmd backend instead of native git one.
+package main
