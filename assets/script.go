1a83915e2cc712982021a2fba40db4378fed4201
Dmitri Shuralyov <shurcooL@gmail.com>
2015-06-21 14:09:47 -0700

    Preserve hash when updating query.

--- assets/script.go
+++ assets/script.go
@@ -234,7 +234,7 @@ func init() {
 		if commitId := document.GetElementByID("commit-id").GetAttribute("title"); query.Get(gtdo.RevisionQueryParameter) != commitId {
 			query.Set(gtdo.RevisionQueryParameter, commitId)
 			// TODO: dom.GetWindow().History().PushState(nil, nil, "#"+element.GetAttribute("data-id"))
-			js.Global.Get("window").Get("history").Call("pushState", nil, nil, "?"+query.Encode())
+			js.Global.Get("window").Get("history").Call("pushState", nil, nil, "?"+query.Encode()+dom.GetWindow().Location().Hash)
 		}
 
 		ke.PreventDefault()
