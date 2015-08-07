fa166b59b836dfde3b673e0a2d8fa36481cee8bd
Dmitri Shuralyov <shurcooL@gmail.com>
2015-08-07 00:44:20 -0700

    Disable updating of vcsstorecmd.
    We're not compatible with latest version of vcsstore, so prevent it
    from being regenerated when running `go generate ./...` as directed
    README.

--- cmd/vcsstorecmd/generate.go
+++ cmd/vcsstorecmd/generate.go
@@ -1,4 +1,6 @@
-//go:generate go run gen.go
+// Generation is disabled for now because we're not compatible with the latest version of vcsstore.
+// Use the currently pinned older version for now.
+//#go:generate go run gen.go
 
 // Command vcsstorecmd is a fork of sourcegraph.com/sourcegraph/vcsstore/cmd/vcsstore,
 // but it uses gitcmd backend instead of native git one.
