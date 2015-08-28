f8304d1c2e8ba6ae00f2253e2b0fca425ef83ea3
Dmitri Shuralyov <shurcooL@gmail.com>
2015-08-27 23:45:13 -0700

    Remove previous html template approach.
    The new code has proven to be robust.

--- import_path_elements_test.go
+++ import_path_elements_test.go
@@ -1,12 +1,7 @@
 package main_test
 
 import (
-	"bytes"
 	"fmt"
-	"html/template"
-	"log"
-	"path"
-	"strings"
 
 	gtdo "github.com/shurcooL/gtdo"
 )
@@ -25,14 +20,8 @@ func ExampleImportPathElementsHtml() {
 	}
 
 	for _, i := range inputs {
-		out1 := gtdo.ImportPathElementsHtml(i.repoImportPath, i.importPath, "")
-		out2 := previousHtmlTemplateApproach(i.repoImportPath, i.importPath)
-
-		if out1 != out2 {
-			log.Printf("out1 != out2\n%q\n%q\n", out1, out2)
-		}
-
-		fmt.Println(out1)
+		html := gtdo.ImportPathElementsHtml(i.repoImportPath, i.importPath, "")
+		fmt.Println(html)
 	}
 
 	// Output:
@@ -42,39 +31,4 @@ func ExampleImportPathElementsHtml() {
 	// <a href="/rsc.io/pdf">rsc.io/pdf</a>/pdfpasswd
 	// io
 	// <a href="/io">io</a>/ioutil
-}
-
-func previousHtmlTemplateApproach(repoImportPath, importPath string) template.HTML {
-	data := struct {
-		ImportPathElements [][2]string // Element name, and full path to element.
-	}{}
-
-	{
-		elements := strings.Split(importPath, "/")
-		elements = elements[len(strings.Split(repoImportPath, "/")):]
-
-		data.ImportPathElements = [][2]string{
-			[2]string{repoImportPath, repoImportPath},
-		}
-		for i, e := range elements {
-			data.ImportPathElements = append(data.ImportPathElements,
-				[2]string{e, repoImportPath + "/" + path.Join(elements[:i+1]...)},
-			)
-		}
-		// Don't link the last element, since it's the current page.
-		data.ImportPathElements[len(data.ImportPathElements)-1][1] = ""
-	}
-
-	t, err := template.New("import-path.html.tmpl").Parse(`{{define "ImportPath"}}{{range $i, $v := .}}{{if $i}}/{{end}}{{if (index $v 1)}}<a href="/{{(index $v 1)}}">{{(index $v 0)}}</a>{{else}}{{(index $v 0)}}{{end}}{{end}}{{end}}`)
-	if err != nil {
-		panic(err)
-	}
-
-	var buf bytes.Buffer
-	err = t.ExecuteTemplate(&buf, "ImportPath", data.ImportPathElements)
-	if err != nil {
-		panic(err)
-	}
-
-	return template.HTML(buf.String())
 }
