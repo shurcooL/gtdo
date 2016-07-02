bda8a0808e39eb303057f3b86ffbb507b48ec880
Dmitri Shuralyov <shurcooL@gmail.com>
2016-07-02 16:58:24 -0700

    Remove obsolete TODO comment.
    It's possible to use vcs.CommitID directly now, but it's not a good
    idea. Importing that package pulls in many unnecessary dependencies,
    increasing the output size. string will do.

--- page/state.go
+++ page/state.go
@@ -6,7 +6,7 @@ type State struct {
 	ImportPath   string
 	RepoSpec     repoSpec
 	ProcessedRev string // ProcessedRev is processed rev; its value is replaced by default branch if empty.
-	CommitID     string // TODO: Either get rid of godep or make gopherjs_http.NewFS use Godeps.json versions, then can start using vcs.CommitID directly.
+	CommitID     string
 }
 
 // TODO: Dedup. But probably by moving it to a common lower level package for types... Not sure if this package is best for it.
