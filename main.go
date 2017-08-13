81fd2e87092509a4d454fa0c7648723ed23927f1
Dmitri Shuralyov <shurcooL@gmail.com>
2017-08-13 00:24:05 -0400

    Specifically disallow all pages for MJ12bot in robots.txt.
    It doesn't seem to parse the Allow: /$ robots.txt syntax correctly and
    ends up trying to index all pages. Disallow all pages as a workaround
    until the bot is fixed.
    
    Also set Content-Type for /robots.txt endpoint.

--- main.go
+++ main.go
@@ -103,8 +103,12 @@ func main() {
 	http.HandleFunc("/", codeHandler)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.HandleFunc("/robots.txt", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
 		io.WriteString(w, `User-agent: *
 Allow: /$
+Disallow: /
+
+User-agent: MJ12bot
 Disallow: /
 `)
 	})
